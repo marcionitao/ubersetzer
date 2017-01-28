c7a0dcea1237d6bcfa85eb8f9a91c20812ff98d1
marcionitao <marcionitao@gmail.com>
2017-01-28 14:18:21 +0100

    Add Functionaly Enter key to txtSuche on Press

--- ubersetzer/Form1.cs
+++ ubersetzer/Form1.cs
@@ -25,18 +25,7 @@ namespace ubersetzer
             // TODO: This line of code loads data into the 'übersetzerDataSet.übersetzer_Consulta' table. You can move, or remove it, as needed.
             this.übersetzer_ConsultaTableAdapter.Fill(this.übersetzerDataSet.übersetzer_Consulta);
 
-            deutschTextBox.Clear();
-            englischTextBox.Clear();
-            spanischTextBox.Clear();
-            russischTextBox.Clear();
-            polnischTextBox.Clear();
-            arabischTextBox.Clear();
-            portugiesischTextBox.Clear();
-            französischTextBox.Clear();
-            italienischTextBox.Clear();
-            türkischTextBox.Clear();
-            rumänischTextBox.Clear();
-            bulgarischTextBox.Clear();
+            this.TxtClear();
 
             this.übersetzerDataGridView.ClearSelection();
             this.übersetzerDataGridView.CurrentCell = null;
@@ -101,10 +90,29 @@ namespace ubersetzer
 
         }
 
-
+        // Method clear all fields and txtSuche
         private void txtSuche_TextChanged(object sender, EventArgs e)
         {
 
+            this.TxtClear();
+
+        }
+
+        // Press ENTER is executable btnSuche_Click to search
+        private void txtSuche_KeyDown(object sender, KeyEventArgs e)
+        {
+            if (e.KeyCode == Keys.Enter)
+            {
+                btnSuche_Click(this, new EventArgs());
+
+                e.Handled = true;
+                e.SuppressKeyPress = true;
+            }
+        }
+
+        // The fields are clear when click in txtSuche to search word
+        public void TxtClear()
+        {
             deutschTextBox.Clear();
             englischTextBox.Clear();
             spanischTextBox.Clear();
@@ -117,8 +125,6 @@ namespace ubersetzer
             türkischTextBox.Clear();
             rumänischTextBox.Clear();
             bulgarischTextBox.Clear();
-
-
         }
 
         private void txtSuche_MouseClick(object sender, EventArgs e)
@@ -137,6 +143,7 @@ namespace ubersetzer
             MessageBox.Show("Die Daten wurden mit Erfolg aufgenommen!");
         }
 
+        // btn to Delete records
         private void DeleteItem_Click(System.Object sender, System.EventArgs e)
         {
 
